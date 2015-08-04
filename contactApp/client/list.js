41bd4191775dd619bd603043f64c3476a495da78
thomas chang <thomascsd808@gmail.com>
2015-08-04 05:29:35 +0000

    Update list update

--- contactApp/client/list.js
+++ contactApp/client/list.js
@@ -4,6 +4,35 @@ Meteor.subscribe('contactList');
 
 Template.list.helpers({
     contacts: function() {
-        return Meteor.Contact.find();
+        return Contacts.find();
+    },
+    showList: function() {
+        var updateMode = Session.get('updateMode') || false;
+        return updateMode ? 'itemDisable' : 'itemVisible';
+    },
+    showEditor: function() {
+        var updateMode = Session.get('updateMode') || false;
+        return updateMode ? 'itemVisible' : 'itemDisable';
+    }
+});
+
+Template.list.events({
+    'click .contactModiy': function(e) {
+        e.preventDefault();
+
+        Session.set('updateMode', true);
+    },
+    'click .update': function(e) {
+        var $container = $(this).parents('.editor');
+
+        e.preventDefault();
+
+        Meteor.call('updateContact', {
+            id: $container.data('oid'),
+            name: $container.find('.contactName').val(),
+            email: $container.find('.email').val(),
+            age: $container.find('.age').val()
+        });
+
     }
 });
