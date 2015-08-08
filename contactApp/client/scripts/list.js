8cf3e308976137c5d428628904869629183d20ef
thomas chang <thomascsd808@gmail.com>
2015-08-08 09:02:09 +0000

    Update contact list

---
+++ contactApp/client/scripts/list.js
@@ -0,0 +1,59 @@
+'use strict';
+
+Meteor.subscribe('contactList');
+
+function getUpdateMode(currentId) {
+    var updateMode = Session.get('updateMode') || false;
+    var selectedId = Session.get('selectedId');
+
+    console.log('currentId:' + currentId);
+    console.log('selectedId:' + selectedId);
+
+    if (currentId === selectedId) {
+        return updateMode;
+    }
+
+    return false;
+}
+
+
+Template.list.helpers({
+    contacts: function() {
+        return Contacts.find();
+    },
+    showList: function() {
+        var updateMode = getUpdateMode(this._id); //Session.get('updateMode') || false;
+        return updateMode ? 'none' : 'block';
+    },
+    showEditor: function() {
+        var updateMode = getUpdateMode(this._id); //Session.get('updateMode') || false;
+        return updateMode ? 'block' : 'none';
+    }
+});
+
+Template.list.events({
+    'click .contactModiy': function(e) {
+        e.preventDefault();
+        Session.set('updateMode', true);
+        Session.set('selectedId', this._id);
+    },
+    'click .contactCancel': function(e) {
+        e.preventDefault();
+        Session.set('updateMode', false);
+        Session.set('selectedId', null);
+    },
+    'click .update': function(e) {
+        var $container = $(e.target).parents('.editor');
+        e.preventDefault();
+
+        Meteor.call('updateContact', {
+            id: this._id,
+            name: $container.find('.contactName').val(),
+            email: $container.find('.email').val(),
+            age: $container.find('.age').val()
+        });
+
+    }
+
+
+});
